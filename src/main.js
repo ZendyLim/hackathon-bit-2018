b1c8229c24606bf6d97a21a5639f92c0f3822a2e
Zendy Lim <zendy.me@gmail.com>
2018-09-13 12:50:36 +0900

    Complete the populate logic

--- src/main.js
+++ src/main.js
@@ -8,7 +8,74 @@ import './assets/styles.scss';
 Vue.config.productionTip = false;
 Vue.use(VueMaterial);
 
+const events = {
+    recommended: [
+        {
+            id: 1,
+            name: 'ABC ABC',
+            date: '23-08-2018',
+            view: 100000,
+            participant: 1510,
+            joined: false
+        },
+        {
+            id: 2,
+            name: 'awehdjkashdjkahdjkasdkjsadhjkhsajkdhasjd',
+            date: '23-08-2018',
+            view: 100000,
+            participant: 1510,
+            joined: false
+        },
+        {
+            id: 3,
+            name: 'assdasjgdhasghdj',
+            date: '23-08-2018',
+            view: 100000,
+            participant: 1510,
+            joined: false
+        }
+    ],
+    popular: [
+        {
+            id: 1,
+            name: 'ABC ABC',
+            date: '23-08-2018',
+            view: 100000,
+            participant: 1510,
+            joined: false
+        },
+        {
+            id: 4,
+            name: 'asd asdqwer  32  rsdasd',
+            date: '23-08-2018',
+            view: 100000,
+            participant: 1510,
+            joined: false
+        },
+        {
+            id: 5,
+            name: 'asdasdasda c xccvsd dfasff',
+            date: '23-08-2018',
+            view: 100000,
+            participant: 1510,
+            joined: false
+        }
+    ],
+    category: [
+        {
+            id: 'iot',
+            name: 'IOT'
+        },
+        {
+            id: 'cooking',
+            name: 'Cooking'
+        }
+    ]
+};
+
+localStorage.setItem('events', JSON.stringify(events));
+
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app');
--- src/views/event/Home.vue
+++ src/views/event/Home.vue
@@ -3,77 +3,43 @@
         <md-list class="md-double-line">
             <md-subheader>Recommended</md-subheader>
 
-            <md-list-item>
+            <md-list-item v-for="rEvent in recommended" :key="rEvent.id">
                 <div>
                     <img src="https://via.placeholder.com/100x60"/>
                 </div>
 
                 <div class="md-list-item-text">
-                    <span>Okutama Hackathonasdsadsadas</span>
+                    <span>{{rEvent.name}}</span>
                     <span>Mobile</span>
                     <span>Mobileasdasdsadsadsadasds</span>
-                </div>
-            </md-list-item>
-
-            <md-list-item>
-                <div>
-                    <img src="https://via.placeholder.com/100x60"/>
-                </div>
-
-                <div class="md-list-item-text">
-                    <span>Okutama Hackathon</span>
-                    <span>Mobile</span>
-                    <span>Mobile</span>
                 </div>
             </md-list-item>
 
             <md-divider></md-divider>
             <md-subheader>Popular</md-subheader>
 
-            <md-list-item>
+            <md-list-item v-for="pEvent in popular" :key="pEvent.id">
                 <div>
                     <img src="https://via.placeholder.com/100x60"/>
                 </div>
 
                 <div class="md-list-item-text">
-                    <span>Okutama Hackathonasdsadsadas</span>
+                    <span>{{pEvent.name}}</span>
                     <span>Mobile</span>
                     <span>Mobileasdasdsadsadsadasds</span>
-                </div>
-            </md-list-item>
-
-            <md-list-item>
-                <div>
-                    <img src="https://via.placeholder.com/100x60"/>
-                </div>
-
-                <div class="md-list-item-text">
-                    <span>Okutama Hackathon</span>
-                    <span>Mobile</span>
-                    <span>Mobile</span>
                 </div>
             </md-list-item>
 
             <md-divider></md-divider>
             <md-subheader>Category</md-subheader>
 
-            <md-list-item>
+            <md-list-item v-for="cat in category" :key="cat.id">
                 <div>
                     <img src="https://via.placeholder.com/100x60"/>
                 </div>
 
                 <div class="md-list-item-text">
-                    <span>Cooking</span>
-                </div>
-            </md-list-item>
-
-            <md-list-item>
-                <div>
-                    <img src="https://via.placeholder.com/100x60"/>
-                </div>
-
-                <div class="md-list-item-text">
-                    <span>IOT</span>
+                    <span>{{cat.name}}</span>
                 </div>
             </md-list-item>
         </md-list>
@@ -82,7 +48,20 @@
 
 <script>
     export default {
-        name: "ChatHome"
+        name: "ChatHome",
+        data: function () {
+            return {
+                recommended: [],
+                popular: [],
+                category: []
+            }
+        },
+        mounted: function () {
+            const events = JSON.parse(localStorage.getItem('events'));
+            this.recommended = events.recommended;
+            this.popular = events.popular;
+            this.category = events.category;
+        }
     }
 </script>
 
